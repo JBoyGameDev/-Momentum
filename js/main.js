886c7ec9b382b8a04f071e0dd9d7e22436c4c10b
James <jmcgamedev001@gmail.com>
2026-03-10 18:13:44 -0400

    fix file locations

---
+++ js/main.js
@@ -0,0 +1,17 @@
+new Phaser.Game({
+    type:   Phaser.AUTO,
+    width:  800,
+    height: 480,
+    backgroundColor: '#04040c',
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+        arcade:  { gravity: { y: 0 }, debug: false },
+    },
+    scene:  [MenuScene, GameScene, UIScene],
+    scale: {
+        mode:       Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    parent: document.body,
+});
